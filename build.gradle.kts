4968981330829017ff1424649453725304e74e45
Akshaansh Singh <71954760+akshaanshs@users.noreply.github.com>
2026-06-15 17:01:35 +0530

    Upgrade google-services plugin to 4.4.1 for Crashlytics compatibility

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ buildscript {
     }
 }
 plugins {
-    id("com.google.gms.google-services") version "4.4.0" apply false
+    id("com.google.gms.google-services") version "4.4.1" apply false
     id("com.google.firebase.firebase-perf") version "1.4.2" apply false
     id("com.google.firebase.crashlytics") version "3.0.2" apply false
     alias(libs.plugins.android.application) apply false
